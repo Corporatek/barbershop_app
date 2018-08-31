a3952bbaac04c1769ddd32cc230a7ad236777046
corporatek <kagwunobi@my.uri.edu>
2018-08-31 11:36:44 -0400

    edit advised by terry

--- app/controllers/barbers_controller.rb
+++ app/controllers/barbers_controller.rb
@@ -41,7 +41,7 @@ class BarbersController < OpenReadController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_barber
-      @barber = Barber.find(params[:id])
+      @barber = current_user.barbers.find(params[:id])
     end
 
     # Only allow a trusted parameter "white list" through.
