57a78396bc26afd83f60e962efcab65b604b08ac
corporatek <kagwunobi@my.uri.edu>
2018-08-31 14:28:59 -0400

    removed phone number

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -71,7 +71,7 @@ class UsersController < ProtectedController
   # private
   def user_creds
     params.require(:credentials)
-          .permit(:email, :password, :password_confirmation, :barber, :phone_number)
+          .permit(:email, :password, :password_confirmation, :barber)
   end
   # private
   def pw_creds
